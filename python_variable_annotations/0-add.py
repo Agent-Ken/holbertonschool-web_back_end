6730fb79d458e8373d1048c990b5cc6e5de1bddf
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-04-13 00:52:01 +0400

    Add files via upload

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -2,6 +2,7 @@
 
 """A basic type-annotated function add() that takes a float a and a float b as arguments."""
 
-def add(a:float, b:float) -> float:
+
+def add(a: float, b: float) -> float:
     """Returns the sum of 2 float numbers."""
     return a + b
