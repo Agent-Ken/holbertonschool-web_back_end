1548719d3f25866dd7ced06bc36c5573625d0e29
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-04-12 23:52:31 +0400

    Add files via upload

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+"""A type-annotated function concat that takes a string str1 and a string str2 as arguments"""
+
+def concat(str1: str, str2: str) -> str:
+    """Returns the concatenation of 2 strings"""
+    return str1+str2
---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""A type-annotated function concat that takes a string str1 and a string str2 as arguments"""
+
+import math
+
+def floor(n: float) -> float:
+    """Returns the concatenation of 2 strings"""
+    return math.floor(n)
