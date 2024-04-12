f302ce774d99b117abee4cf4b9519ad8e7c9c4bd
Agent-Ken <128805216+Agent-Ken@users.noreply.github.com>
2024-04-13 00:51:08 +0400

    Update 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""A basic type-annotated function add() that takes a float a and a float b as arguments"""
+"""A basic type-annotated function add() that takes a float a and a float b as arguments."""
 
 def add(a:float, b:float) -> float:
     """Returns the sum of 2 float numbers."""
