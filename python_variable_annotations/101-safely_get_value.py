1a213979d154e1d5eab2a6ca4be71b1ae558b41f
agent-ken <gbp.kennenson@gmail.com>
2024-04-13 21:22:47 +0000

    test

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 """A simple Python module with type annotations."""
+from typing import Any, Mapping, Union, TypeVar
 
-from typing import Mapping, TypeVar, Any
 T = TypeVar('T')
+R = Union[Any, T]
+D = Union[T, None]
 
 
-def safely_get_value(dct: Mapping[Any, T], key: Any, default: T = None) -> T:
-    """Retrieve the value for a given key from a dictionary safely.
+def safely_get_value(dct: Mapping, key: Any, default: D = None) -> R:
+    """
+    Retrieve the value for a given key from a dictionary safely.
     Returns:
         The value associated with the key if it exists, otherwise the default.
     """
