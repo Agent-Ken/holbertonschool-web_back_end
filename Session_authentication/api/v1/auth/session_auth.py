d4c72dc48bc4c8467f91f46668cb70e43fda9581
agent-ken <gbp.kennenson@gmail.com>
2024-04-21 19:18:46 +0000

    update

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -3,11 +3,21 @@
 """
 
 from api.v1.auth.auth import Auth
+from typing import Dict
+import uuid
 
 
 class SessionAuth(Auth):
     """ Inherited from Auth
     """
+    user_id_by_session_id: Dict = {}
 
-    def __init__(self):
-        super().__init__()
+    def create_session(self, user_id: str = None) -> str:
+        """ Session ID creation
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id: str = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        return session_id
