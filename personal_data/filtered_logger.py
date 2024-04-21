51d98694008fdd901bd0b40e6cafdad64ecee84b
agent-ken <gbp.kennenson@gmail.com>
2024-04-21 13:15:20 +0000

    update

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -57,9 +57,9 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     '''Returns: connector to the db'''
     connection = mysql.connector.connection.MySQLConnection(
         user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
-        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', 'dbpassw96'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
         host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
-        database=os.getenv("PERSONAL_DATA_DB_NAME", "personal_hb_db")
+        database=os.getenv("PERSONAL_DATA_DB_NAME", '')
     )
     return connection
 
