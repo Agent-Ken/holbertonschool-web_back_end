b3c6f03d4553e8ff0f8e993efb66452f48b97817
agent-ken <gbp.kennenson@gmail.com>
2024-04-21 17:36:46 +0000

    update

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -17,7 +17,14 @@ class Auth:
         """ Method returns True
         if the path is not in the list
         """
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        for exclude_path in excluded_paths:
+            if path.strip("/") in exclude_path.strip("/"):
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method that returns None
