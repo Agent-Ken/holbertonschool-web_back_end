bcb9369d0245b63a6346b2ed71e0327c16cb564c
agent-ken <gbp.kennenson@gmail.com>
2024-04-21 19:49:20 +0000

    update

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-""" Session Auth
+""" Module of Session Authentication
 """
 
 from api.v1.auth.auth import Auth
-from typing import Dict
+from typing import Dict, TypeVar
+from models.user import User
 import uuid
 
 
 class SessionAuth(Auth):
-    """ Inherited from Auth
+    """ Session Authentication Class inherited from Auth
     """
     user_id_by_session_id: Dict = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ Session ID creation
+        """ Create a Session ID
         """
         if user_id is None or type(user_id) is not str:
             return None
@@ -23,9 +24,18 @@ class SessionAuth(Auth):
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ Return User ID
+        """ Return a User ID based on a Session ID
         """
         if session_id is None or type(session_id) is not str:
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ current_user method that returns a User instance
+        """
+        cookie: str = self.session_cookie(request)
+        user_id: str = self.user_id_for_session_id(cookie)
+        user: TypeVar('User') = User.get(user_id)
+
+        return user
