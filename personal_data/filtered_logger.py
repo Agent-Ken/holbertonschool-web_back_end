561aea2d48831c903207eacf3763d71d1bc2e2dd
agent-ken <gbp.kennenson@gmail.com>
2024-04-21 12:41:19 +0000

    update

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,6 +6,8 @@ This module provides a function to obfuscate log messages.
 import re
 from typing import List
 import logging
+import mysql.connector
+import os
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
@@ -49,3 +51,14 @@ def get_logger() -> logging.Logger:
     stream.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(stream)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    '''Returns: connector to the db'''
+    connection = mysql.connector.connection.MySQLConnection(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv("PERSONAL_DATA_DB_NAME")
+    )
+    return connection
