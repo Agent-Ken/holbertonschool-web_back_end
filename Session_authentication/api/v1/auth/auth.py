abef506ede90b3fe6afd26b87877b7aa405d7227
agent-ken <gbp.kennenson@gmail.com>
2024-04-21 19:12:36 +0000

    create

---
+++ Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+""" Authorization module
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """ Auth class
+    """
+
+    def __init__(self):
+        """ Auth class constructor
+        """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Method returns True if the path
+        is not in the list of excluded paths
+        """
+
+        if path is None or not excluded_paths:
+            return True
+
+        path = path.strip('/')
+
+        for pattern in excluded_paths:
+            pattern = pattern.strip('/')
+            if pattern.endswith('*'):
+                if path.startswith(pattern[:-1]):
+                    return False
+            else:
+                if path == pattern:
+                    return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ Method that returns None
+        """
+        if request is None:
+            return None
+
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ Method returns None
+        """
+        return request
