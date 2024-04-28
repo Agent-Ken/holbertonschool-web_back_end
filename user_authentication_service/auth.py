57ed26d1bd50fcce8164f7766223c801dbdde89b
agent-ken <gbp.kennenson@gmail.com>
2024-04-28 08:06:43 +0000

    update

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -2,23 +2,22 @@
 """
 Auth module
 """
+import bcrypt
 from db import DB
 from user import User
-import bcrypt
-
-
-def _hash_password(password: str) -> bytes:
-    """Hash a password
-    """
-    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
 
 class Auth:
-    """class for interacting with the authentication DB.
+    """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """Hash a password
+        """
+        return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
     def register_user(self, email: str, password: str) -> User:
         """register a new user
@@ -27,6 +26,6 @@ class Auth:
         if user:
             raise ValueError(f'User {email} already exists')
         else:
-            hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+            hashed_password = self._hash_password(password)
             new_user = self._db.add_user(email, hashed_password)
             return new_user
