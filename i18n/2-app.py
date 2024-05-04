4bdecf118203dc708c56c051ad0dd8551f5071c8
agent-ken <gbp.kennenson@gmail.com>
2024-05-04 22:26:46 +0000

    create

---
+++ i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+This module adds locale selection from requests.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Configuration class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ language translation """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """ a simple template with
+    a welcome message. """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
