0a00a1baa18239e7087112e85f1746036ce7d06d
agent-ken <gbp.kennenson@gmail.com>
2024-05-04 22:17:17 +0000

    create

---
+++ i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+This module sets up Flask with Flask-Babel
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Configuration class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """ simple template with
+    a welcome message. """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
