827773f0ce06fb20ee01dceb619f3721e3d16b38
agent-ken <gbp.kennenson@gmail.com>
2024-05-05 17:55:12 +0000

    create

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Unit tests for utils.py
+"""
+
+import unittest
+from utils import access_nested_map
+from parameterized import parameterized
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ test class
+    """
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, result):
+        """ test access_nested_map
+        """
+        self.assertEqual(access_nested_map(nested_map, path), result)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ test the exception
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
