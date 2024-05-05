d1abaf89e2b426b3f5cda77e9e8f601679fd8552
agent-ken <gbp.kennenson@gmail.com>
2024-05-05 15:58:01 +0000

    create

---
+++ i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+This module sets up Flask with Flask-Babel for internationalization
+and parametrizes templates using message IDs.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+
+class Config:
+    """ Configuration class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ Select a language translation """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """ template with parametrized text
+    render by using message IDs. """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
