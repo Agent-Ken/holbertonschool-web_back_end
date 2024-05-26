06f90366fa816e47f4bf582cc4c9c6287f3fb4fb
agent-ken <gbp.kennenson@gmail.com>
2024-05-26 18:35:40 +0000

    create

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Provides stats about Nginx logs stored in MongoDB"""
+
+from pymongo import MongoClient
+
+def log_stats():
+    """print stats about Nginx logs"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    logs_count = nginx_collection.count_documents({})
+    print(f"{logs_count} logs")
+    
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        count = nginx_collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
+    status_check = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
+
+
+if __name__ == "__main__":
+    log_stats()
