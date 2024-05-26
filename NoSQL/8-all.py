4ef48fd5da75068d764329f79a5c4e19b389a50b
agent-ken <gbp.kennenson@gmail.com>
2024-05-26 18:03:29 +0000

    update

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 """List all documents in a collection"""
 
-from pymongo
+from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
     """List all documents in a collection"""
     documents = mongo_collection.find()
-    return [doc for doc in documents] if documents else []
+    if mongo_collection.count() == 0:
+        return []
+
+    return list(mongo_collection)
