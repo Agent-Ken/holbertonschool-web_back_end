643ad9d0d1248b17fcc7c2b477798d4c72ed9052
agent-ken <gbp.kennenson@gmail.com>
2024-06-02 19:42:21 +0000

    upload

---
+++ ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+// Implementing a class ClassRoom 
+export default class ClassRoom {
+  constructor(maxStudentsSize = 0) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
---
+++ ES6_classes/100-evcar.js
@@ -0,0 +1,15 @@
+// Implementing a class EVCar that extends class Car
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+}
+
+export default EVCar;
