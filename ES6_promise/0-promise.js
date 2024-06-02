7aae608335d4fb3e637945e0ff5e6fba4cceec59
agent-ken <gbp.kennenson@gmail.com>
2024-06-02 19:58:33 +0000

    test

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,13 +1,14 @@
 // function that resolves or rejects 
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    /* eslint-disable */
-    if (true) {
-        resolve();
-    } else {
-        reject();
-    }
-    /* eslint-enable */
+    setTimeout(() => {
+      const success = Math.random() > 0.5;
+      if (success) {
+        resolve({ data: 'This is a simulated successful response' });
+      } else {
+        reject(new Error('This is a simulated error'));
+      }
+    }, 1000);
   });
 }
 
