d420e047404fad89385459e9fad83134b99154a2
agent-ken <gbp.kennenson@gmail.com>
2024-06-02 19:17:13 +0000

    update

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,6 +1,6 @@
-// Reject the promises
+// Reject promise
 export default function uploadPhoto(filename) {
-    return Promise.reject(
-        new Error(`${filename} cannot be processed`),
-    );
+  return Promise.reject(
+    new Error(`${filename} cannot be processed`),
+  );
 }
