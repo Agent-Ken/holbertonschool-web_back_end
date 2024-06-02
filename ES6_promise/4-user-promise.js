c16a87e76cda242cd6d5064e3fb8cbfed29ea811
agent-ken <gbp.kennenson@gmail.com>
2024-06-02 18:44:19 +0000

    upload

---
+++ ES6_promise/4-user-promise.js
@@ -0,0 +1,11 @@
+// Sign the user up
+function signUpUser(firstName, lastName) {
+    return Promise.resolve(
+        {
+            firstName,
+            lastName,
+        },
+    );
+}
+
+export default signUpUser;
---
+++ ES6_promise/8-try.js
@@ -0,0 +1,10 @@
+// Error of dividing by 0
+export default function divideFunction(numerator, denominator) {
+    if (denominator === 0) {
+        throw Error('cannot divide by 0');
+    }
+
+    const result = numerator / denominator;
+
+    return result;
+}
