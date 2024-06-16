277eddbf3b165e38256f45f996e599352ad53970
agent-ken <gbp.kennenson@gmail.com>
2024-06-16 18:43:33 +0000

    update

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,10 @@
 const createInt8TypedArray = (length, position, value) => {
-  if (position >= length || position < 0) {
-    throw new Error('Position outside range');
-  }
-  
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
+  if (position > length - 1) {
+    throw new Error('Position outside range');
+  }
   view.setInt8(position, value);
-  
   return view;
 };
 
