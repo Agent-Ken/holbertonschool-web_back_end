8cb5cee172b1978362c62fc90faff96b8c4c8392
agent-ken <gbp.kennenson@gmail.com>
2024-06-16 18:42:01 +0000

    update

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,22 @@
 const updateStudentGradeByCity = (students, city, newGrades) => {
-  return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const studentGrade = newGrades.find(grade => grade.studentId === student.id);
-      return {
-        ...student,
-        grade: studentGrade ? studentGrade.grade : 'N/A',
-      };
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+
+  const studentByCity = students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const studentGrade = newGrades.filter((note) => student.id === note.studentId);
+      let grade = 'N/A';
+
+      if (studentGrade[0]) {
+        grade = studentGrade[0].grade;
+      }
+
+      return { ...student, grade };
     });
+
+  return studentByCity;
 };
 
 export default updateStudentGradeByCity;
