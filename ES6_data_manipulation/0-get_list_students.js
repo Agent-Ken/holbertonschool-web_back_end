1b3efc5d41f7a7e30c9149b256880e9898577835
agent-ken <gbp.kennenson@gmail.com>
2024-06-16 17:54:54 +0000

    upload

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+// Get student list
+
+const getListStudents = () => [
+  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  { id: 2, firstName: 'James', location: 'Columbia' },
+  { id: 5, firstName: 'Serena', location: 'San Francisco' },
+];
+
+export default getListStudents;
