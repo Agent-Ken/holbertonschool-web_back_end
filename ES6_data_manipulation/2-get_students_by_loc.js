f6228197fa0a99da4fd948f482e8e27b95d9ca79
agent-ken <gbp.kennenson@gmail.com>
2024-06-16 18:29:43 +0000

    linting rules

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,6 +1,6 @@
 const getStudentsByLocation = (list, city) => {
-    const students = list.filter((item) => item.location === city);
-    return students;
+  const students = list.filter((item) => item.location === city);
+  return students;
 };
 
 export default getStudentsByLocation;
